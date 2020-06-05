509a17271f27ea42bac51547209889edb0c505da
Liming Teng <limingteng@ihl-lap23-mac.local>
2020-06-05 13:23:00 -0700

    server update

---
+++ Server/Auth/src/main/java/cn/trunch/auth/service/RegisterService.java
@@ -0,0 +1,27 @@
+package cn.trunch.auth.service;
+
+import cn.trunch.auth.dao.UserDao;
+import cn.trunch.auth.entity.Message;
+import cn.trunch.auth.entity.User;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class RegisterService {
+
+    @Resource
+    UserDao userDao;
+
+    public Message checkUserExist(String userId) {
+        User user = userDao.getUserInfo(userId);
+        if (null != user) {
+            return new Message(202, "注册失败", new User());
+        }
+    }
+
+    public Message addUserInfo(HttpServletRequest request) {
+
+    }
+}
